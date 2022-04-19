0b93dd14a57dc90c747f42c86a21862d9ade92ca
Eduardo Lima <58784670+Ceduardosl@users.noreply.github.com>
2022-04-18 21:48:46 -0300

    Add files via upload

---
+++ Genetic_Algorithm.py
@@ -0,0 +1,25 @@
+#%%
+import numpy as np
+import time
+
+def fitness_measure(inputs, pop):
+    #Cálculo da aptidão de cada indíviduo contido na população
+    fitness = np.sum(pop*inputs, axis = 1)
+
+    return (fitness)
+
+
+
+#%%
+if __name__ == "__main__":
+    # Y = w1x1 + w2x2 + w3x3 + w4x4 + w5x5 + w6x6
+    inputs = [4, -2, 3.5, 5, -11, -4.7]
+    
+    genes = 6 #Coeficientes para otimizar
+
+    num_chrom = 8 #num chromossomes
+ 
+    pop = np.random.uniform(low = -4, high = 4, size = (8,6))
+
+    print(fitness_measure(inputs, pop))
+#%%
